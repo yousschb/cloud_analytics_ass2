3ef557ac7629a71af439431dd03b05faf7a9bb9b
yousschb <149575962+yousschb@users.noreply.github.com>
2024-04-22 06:15:34 +0200

    Update movie_selection.py

--- MovieApp/movie_selection.py
+++ MovieApp/movie_selection.py
@@ -11,39 +11,6 @@ if 'movie_title_selected' not in st.session_state:
     st.session_state['movie_title_selected'] = list()
 
 st.set_page_config(page_title="Movie Recommendation", layout="wide", initial_sidebar_state="expanded")
-    
-"""
-
-st.set_page_config(page_title="Movie Recommendation")
-st.markdown("<h1 style='text-align: center;'>Movie Recomendation</h1>", unsafe_allow_html=True)
-
-st.title("Movie Selection")
-
-
-# elastic search autocomplete search
-movies_query = st.text_input("Search for a movie title here")
-if movies_query:
-    st.write("Results  (Click To Select):")
-    autocomplete_results = get_data_from_flask(f"elastic_search/{movies_query}")
-    if autocomplete_results:
-        for i in autocomplete_results:
-            button_key = f"select_{i}"
-            if st.button(f"{i}", key=button_key, type="primary"):
-                if i not in st.session_state['movie_title_selected']:
-                    st.session_state['movie_title_selected'].append(i)
-
-
-# display selected movies in sidebar and a button to remove them 
-if st.session_state["movie_title_selected"]:
-    st.sidebar.write("You have selected these movies (Click to remove):")
-    for i in st.session_state["movie_title_selected"]:
-        if st.sidebar.button(f"{i}", key=f"remove_{i}", type="secondary"):
-            st.session_state["movie_title_selected"].remove(i)
-            st.rerun()
-    if st.sidebar.button("Get Recommendations", type= "primary"):
-        st.switch_page('pages/movie_recommendation.py')
-        """
-
 
 # Style personnalisé pour améliorer l'esthétique de la page
 st.markdown("""
