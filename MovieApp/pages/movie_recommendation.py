0921266feb65db2981396d4af82cc79201ead964
yousschb <149575962+yousschb@users.noreply.github.com>
2024-04-29 02:02:20 +0200

    Update movie_recommendation.py

--- MovieApp/pages/movie_recommendation.py
+++ MovieApp/pages/movie_recommendation.py
@@ -33,9 +33,17 @@ def fetch_flask_data(url_path):
     base_url = "https://cloud-analytics-ass213-gev3pcymxa-uc.a.run.app"
     url = f"{base_url}{url_path if url_path.startswith('/') else '/' + url_path}"
     response = requests.get(url)
-    
+    if response.status_code == 200:
+        try:
+            return response.json()
+        except ValueError:
+            st.error("Invalid JSON response")
+            return None
+    else:
+        st.error(f"Failed to fetch data: HTTP status code {response.status_code}")
+        return None
 
-def title_from_id(id):
+def get_title_from_id(id):
     """Retrieve movie title using movie ID."""
     df = pd.DataFrame(fetch_flask_data("title_from_movie_id/" + str(id)), columns=["title"])
     return df
@@ -94,7 +102,7 @@ if st.session_state['movie_title_selected']:
     for movie_id in top_movies['movieId']:
         if movie_id not in st.session_state['movie_title_selected']:
             tmdb_id = fetch_flask_data("tmdb_id/" + str(movie_id))
-            with st.expander(f"{title_from_id(movie_id)['title'][0]}"):
+            with st.expander(f"{get_title_from_id(movie_id)['title'][0]}"):
                 display_info(tmdb_id["tmdbId"]["0"])
 
 # Interaction buttons
